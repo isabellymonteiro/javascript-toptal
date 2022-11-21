6edc71ca5e2c849dca531912f4a2e46eb4128b10
Isabelly Monteiro <isabelly.monteiro@toptal.com>
2022-11-21 18:04:16 -0300

    added exercism protein translation solution

---
+++ js-level-1/extra-exercises/exercism.org/protein-translation.js
@@ -0,0 +1,40 @@
+// https://exercism.org/tracks/javascript/exercises/protein-translation
+
+const proteins = {
+  AUG: 'Methionine',
+  UUU: 'Phenylalanine',
+  UUC: 'Phenylalanine',
+  UUA: 'Leucine',
+  UUG: 'Leucine',
+  UCU: 'Serine',
+  UCC: 'Serine',
+  UCA: 'Serine',
+  UCG: 'Serine',
+  UAU: 'Tyrosine',
+  UAC: 'Tyrosine',
+  UGU: 'Cysteine',
+  UGC: 'Cysteine',
+  UGG: 'Tryptophan',
+  UAA: 'STOP',
+  UAG: 'STOP',
+  UGA: 'STOP'
+}
+
+export const translate = (rna) => {
+  if (!rna) return []
+  const codons = rna.match(/.{1,3}/g)
+  const translatedRna = []
+
+  for (let codon of codons) {
+    codon = codon.toUpperCase()
+
+    if (proteins[codon] === 'STOP') break
+    if (proteins[codon]) {
+      translatedRna.push(proteins[codon])
+    } else {
+      throw Error("Invalid codon")
+    }
+  }
+
+  return translatedRna
+}
