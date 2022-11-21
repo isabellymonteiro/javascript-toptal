5f47de07907c146d6ccd800f04af2af64c6e4d5a
Isabelly Monteiro <isabelly.monteiro@toptal.com>
2022-11-21 16:39:32 -0300

    added exercism flatten array solution

---
+++ js-level-1/extra-exercises/exercism.org/flatten-array.js
@@ -0,0 +1,23 @@
+// https://exercism.org/tracks/javascript/exercises/flatten-array
+
+// primary solution
+export const flatten = (list) => {
+  return list.reduce((total, curr) => {
+    if (curr === null) return total
+    if (Array.isArray(curr)) return total.concat(flatten(curr))
+    return total.concat(curr)
+  }, [])
+}
+
+/* secondary solution
+const flatten = (list) => {
+  const flattenedList = []
+
+  list.forEach(item => {
+    if (item === null) return
+    Array.isArray(item) ? flattenedList.push(...flatten(item)) : flattenedList.push(item)
+  })
+
+  return flattenedList
+} 
+*/
